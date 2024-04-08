27b54477b2a5e122a73e42484e4e239d02386ef2
Paulkra01 <krakis007@gmail.com>
2024-04-08 16:20:39 +0200

    update main file

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-# test
+import matplotlib.pyplot as plt
+from load_data import load_data
+
+
+data = load_data('activity.csv')
+power_unsorted = data["PowerOriginal"]
+n = len(power_unsorted)
+
+x_values = range(n)
+
+
+# Plot the data
+plt.plot(x_values, power_unsorted)
+
+# Add labels and title
+plt.xlabel('Index')
+plt.ylabel('Power Number')
+plt.title('Plot of Power Numbers')
+
+# Display the plot
+plt.grid(True)
+plt.show()
