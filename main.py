dbb36cd632b42c5731270fbab9401e089a46f7af
Paulkra01 <krakis007@gmail.com>
2024-04-10 09:31:40 +0200

    add savefig in main

--- main.py
+++ main.py
@@ -13,10 +13,11 @@ x_values = range(n)
 plt.plot(x_values, power_unsorted)
 
 # Add labels and title
-plt.xlabel('Index')
-plt.ylabel('Power Number')
-plt.title('Plot of Power Numbers')
+plt.xlabel('time in seconds')
+plt.ylabel('power in watts')
+plt.title('workout')
 
 # Display the plot
 plt.grid(True)
+plt.savefig('power_graph.png')
 plt.show()
