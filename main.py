23b8b3535cc816f9b38596c6b41097c7ed02cb36
Paulkra01 <krakis007@gmail.com>
2024-04-12 14:02:38 +0200

    add figures folder

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import matplotlib.pyplot as plt
 from load_data import load_data
 
@@ -9,15 +10,26 @@ n = len(power_unsorted)
 x_values = range(n)
 
 
-# Plot the data
+# plot the data
 plt.plot(x_values, power_unsorted)
 
-# Add labels and title
+# add labels and title
 plt.xlabel('time in seconds')
 plt.ylabel('power in watts')
 plt.title('workout')
 
-# Display the plot
+# display the plot
 plt.grid(True)
 plt.savefig('power_graph.png')
+
+# create folder "figures"
+figures_folder = 'figures'
+if not os.path.exists(figures_folder):
+    os.makedirs(figures_folder)
+
+# Save the picture
+plt.savefig(os.path.join(figures_folder, 'power_graph.png'))
+
+
+
 plt.show()
